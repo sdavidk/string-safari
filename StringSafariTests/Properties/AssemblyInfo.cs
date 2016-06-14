db79f58b047c5c368e6f17717c038cc51c14be21
James M. Allen <james.m.allen@gmail.com>
2016-06-14 17:39:32 -0400

    Initial commit

---
+++ StringSafariTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringSafariTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringSafariTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4469d36-240a-4e8d-a685-9af15c10244b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringSafariTests/StringSafariTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringSafari.Test
+{
+    [TestFixture]
+    public class StringSafariTest
+    {
+        [TestCase("zebra", ExpectedResult = true)]
+        [TestCase("ZEBRA", ExpectedResult = false)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = true)]
+        public bool BabyZebraSearch(string str)
+        {
+            return SafariGuide.HasBabyZebra(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = false)]
+        [TestCase("ZEBRA", ExpectedResult = true)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = false)]
+        public bool AdultZebraSearch(string str)
+        {
+            return SafariGuide.HasAdultZebra(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = true)]
+        [TestCase("ZEBRA", ExpectedResult = true)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = true)]
+        public bool ZebraSearch(string str)
+        {
+            return SafariGuide.HasZebra(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = false)]
+        [TestCase("ZEBRA", ExpectedResult = false)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = true)]
+        [TestCase("zebra LION ZEBRA lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = false)]
+        public bool DazzleSearch(string str)
+        {
+            return SafariGuide.HasADazzle(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = false)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = true)]
+        [TestCase("zebra LION ZEBRA lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = true)]
+        [TestCase("LION zebra lion", ExpectedResult = true)]
+        public bool PrideSearch(string str)
+        {
+            return SafariGuide.HasAPride(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = false)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = false)]
+        [TestCase("zebra LION ZEBRA lion", ExpectedResult = false)]
+        [TestCase("LION zebra LION", ExpectedResult = true)]
+        [TestCase("LION zebra lion", ExpectedResult = true)]
+        public bool BloodSearch(string str)
+        {
+            return SafariGuide.ThereWillBeBlood(str);
+        }
+
+        [TestCase("zebra", ExpectedResult = true)]
+        [TestCase("Lion", ExpectedResult = false)]
+        [TestCase("LION ZEBRA zebra lion", ExpectedResult = false)]
+        [TestCase("zebra LION ZEBRA lion", ExpectedResult = true)]
+        [TestCase("LION zebra LION", ExpectedResult = false)]
+        [TestCase("LION zebra lion", ExpectedResult = false)]
+        public bool SafeDistanceTest(string str)
+        {
+            return SafariGuide.SafeDistanceToLion(str);
+        }
+
+    }
+}
